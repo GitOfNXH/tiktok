3c5e3f593691787738e84a8d45e6f26e16288cd8
hieu nguyen <hieun310102@gmail.com>
2024-09-21 16:36:49 +0700

    Xay dung UI Header #1

--- src/Components/Layouts/Components/Header/index.js
+++ src/Components/Layouts/Components/Header/index.js
@@ -1,12 +1,30 @@
 import classNames from 'classnames/bind'
 import style from './Header.module.scss'
+import image from '~/assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 const cx = classNames.bind(style)
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}>Header</div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={image.logo} alt='Tiktok' />
+                </div>
+                <div className={cx('search')}>
+                    <input type='text' placeholder='Search' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <div className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </div>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </header>
     )
 }
